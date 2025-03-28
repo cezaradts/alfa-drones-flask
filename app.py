6d036c4c31bb5ad65f1aa0aa4c06fc7586084014
cezaradts <cezaradts@gmail.com>
2025-03-27 22:57:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Contato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    telefone = db.Column(db.Integer(100))
+    telefone = db.Column(db.String(20))
     mensagem = db.Column(db.Text)
 
 
